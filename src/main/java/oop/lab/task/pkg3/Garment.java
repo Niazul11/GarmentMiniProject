36f95b08bbe32e9106814fd863c2ec07f8724a5e
Niazul11 <naim23105101360@diu.edu.bd>
2024-11-01 19:19:36 +0600

    Added Fabric & Supplier Class

--- src/main/java/oop/lab/task/pkg3/Garment.java
+++ src/main/java/oop/lab/task/pkg3/Garment.java
@@ -24,3 +24,32 @@ class Garment {
         return discount;
     }
 }
+class Fabric {
+
+    public String id;
+    public String type;
+    public String color;
+    public double pricePerMeter;
+
+    double calculateCost(double meters) {
+        double newPrice = pricePerMeter * meters;
+        return newPrice;
+    }
+}
+
+class Supplier {
+
+    public String id;
+    public String name;
+    public String contactInfo;
+    //List
+    List<Fabric> suppliedFabric = new ArrayList<>();
+
+    void addFabric(Fabric fabric) {
+        suppliedFabric.add(fabric);
+    }
+
+    List<Fabric> getSuppliedFabrics() {
+        return suppliedFabric;
+    }
+}
